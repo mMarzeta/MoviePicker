b5e1c2b3e770df716bc3970e56237c7cbc43eb78
szymonSlonina <szymon.slonina@gmail.com>
2017-09-11 00:03:50 +0200

    added gunicorn support

--- movie_picker/movie_picker/settings.py
+++ movie_picker/movie_picker/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -87,6 +88,10 @@ DATABASES = {
         'PORT' : '5432',
     }
 }
+
+#for configuration to heroku
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
